a30e1d249546a86760488ec2cac3fc8c3109ac9d
was-kamil <was-kamil@wp.pl>
2019-12-20 22:33:04 +0100

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Simple user test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+
+// Testing Calculator class
+
+        Calculator calcula = new Calculator ();
+
+        int addResult = calcula.addAToB(100, 50);
+        int subtractResult = calcula.subAFromB(100, 50);
+
+        if (addResult == 150 && subtractResult == 50) {
+            System.out.println("Calculator test passed.");
+        } else {
+            System.out.println("Calculator test failed.");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subAFromB(int a, int b) {
+        return a - b;
+    }
+
+}
