9d2328fc78c86bbd9640bc668ce426f4967fb170
was-kamil <was-kamil@wp.pl>
2020-02-29 22:04:46 +0100

    14.2

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Circle.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Circle.java
@@ -1,4 +1,9 @@
 package com.kodilla.spring.intro.shape;
 
-public class Circle {
+public class Circle implements Shape {
+    @Override
+    public String draw() {
+        System.out.println("This is a circle");
+        return "This is a circle";
+    }
 }
--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Triangle.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Triangle.java
@@ -1,4 +1,9 @@
-package com.kodilla.spring.intro;
+package com.kodilla.spring.intro.shape;
 
-public class Triangle {
+public class Triangle implements Shape {
+    @Override
+    public String draw() {
+        System.out.println("This is a triangle");
+        return "This is a triangle";
+    }
 }
