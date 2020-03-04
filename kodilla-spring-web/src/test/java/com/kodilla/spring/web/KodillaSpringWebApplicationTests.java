dc16624af0d9c4d9660b770e7007ac5d81197d10
was-kamil <was-kamil@wp.pl>
2020-03-04 05:55:28 +0100

    14.4

---
+++ kodilla-spring-web/src/test/java/com/kodilla/spring/web/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
