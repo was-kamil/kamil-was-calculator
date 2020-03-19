5661f2094aceba342c5a5ef5a729c1df88ccf2f4
was-kamil <was-kamil@wp.pl>
2020-03-20 00:29:32 +0100

    16.5 - 16.6

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -39,4 +39,30 @@ public class DbManagerTestSuite {
         statement.close();
         Assert.assertEquals(5, counter);
     }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+
+        //When
+        String sqlQuery =   "SELECT U.FIRSTNAME, U.LASTNAME\n" +
+                            "FROM USERS U, POSTS P\n" +
+                            "WHERE U.ID = P.USER_ID\n" +
+                            "GROUP BY USER_ID\n" +
+                            "HAVING COUNT(*) >= 2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+
+        //Then
+        int counter = 0;
+        while(rs.next()) {
+            System.out.println(rs.getString("FIRSTNAME") + ", " +
+                                rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(2, counter);
+    }
 }
